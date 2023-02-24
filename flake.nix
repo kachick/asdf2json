bcf0f0376e8e612163fc872afa3ce5b6dcb864b0
Kenichi Kamiya <kachick1@gmail.com>
2023-02-24 16:03:21 +0900

    Fix cache logics

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-    inputs = {
+  inputs = {
     nixpkgs = { url = "https://github.com/NixOS/nixpkgs/archive/e521a883757b06720d28d4a303e0b4f7f0d7fcfc.tar.gz"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
   };
